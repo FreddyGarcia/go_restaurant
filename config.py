d0a7f35f6da7a2d7371e58d9bcada9abd1cb27d7
Freed <buscando_alink@hotmail.com>
2017-07-08 22:47:09 -0400

    Change database from postgres to mysql

--- config.py
+++ config.py
@@ -7,21 +7,23 @@ Debug = True
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DB_CONFIG = {
-	'USER' : 'postgres',
-	'PASS' : 'toor',
-	'HOST' : 'localhost',
-	'NAME' : 'postgres',
-	'PORT' : 5432
+    'PROTOCOL': 'mysql+pymysql',
+    'HOST': '127.0.0.1',
+    'USER': 'root',
+    'PASS': 'toor',
+    'PORT': '3306',
+    'DB': 'go_restaurant'
 }
 
 # Database config
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{USER}:{PASS}@{HOST}:{PORT}/{NAME}'.format(**DB_CONFIG)
+SQLALCHEMY_DATABASE_URI = "{PROTOCOL}://{USER}:{PASS}@{HOST}/{DB}".format(**DB_CONFIG)
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{USER}:{PASS}@{HOST}:{PORT}/{NAME}'.format(**DB_CONFIG)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
 
 # Mapbox config
 MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoiem9yZW4xMDEiLCJhIjoiY2oza3ZtOHJlMDB2ZzJ3bjN2OGVoandzciJ9.-dWEZBmKwQvfkzsh3c72wA'
-MAPBOX_MAP_ID  = 'mapbox.satellite'
+MAPBOX_MAP_ID  = 'zoren101.cj3n1pd1k002x33ojv3fcogoq-1eqxa'
 
 # Session key
 SECRET_KEY = 'secret'
